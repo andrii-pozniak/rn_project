2c8143f39ad02dfaccdc39ff4d02c795398407cf
andrii-pozniak <andriipozniak@gmail.com>
2023-02-25 23:47:15 +0200

    add form

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -1,33 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={require("./assets/images/photo_bg.jpg")} style={styles.image}>
-      <Text style={styles.text}>Registration</Text>
-        {/* <View style={styles.form}>
-        <Text style={styles.text}>Registration</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Login"
-        // onChangeText={onChangeText}
-        // value={text}
-        />
-        {/* <TextInput
-          style={styles.input}
-          placeholder="Email"
-        // onChangeText={onChangeText}
-        // value={text}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-        // onChangeText={onChangeText}
-        // value={text}
-        /> */}
-        {/* </View> */}
-         {/* */} 
+
+        <View style={styles.form}>
+          <View style={styles.photo}></View>
+          <Text style={styles.text}>Registration</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Login"
+          // onChangeText={onChangeText}
+          // value={text}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+          // onChangeText={onChangeText}
+          // value={text}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+          // onChangeText={onChangeText}
+          // value={text}
+          />
+        </View>
+
       </ImageBackground>
     </View>
   );
@@ -43,7 +44,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 30,
     fontWeight: 500,
-    marginBottom: 33,    
+    marginBottom: 33,
   },
   image: {
     flex: 1,
@@ -53,20 +54,35 @@ const styles = StyleSheet.create({
     height: 50,
     width: 343,
     marginBottom: 16,
+    marginLeft: 16,
+
     backgroundColor: '#F6F6F6',
     borderColor: '#E8E8E8',
     borderWidth: 1,
     borderRadius: 8,
     padding: 16,
-    fontSize: 16,
-    fontWeight: 400,
+
   },
   form: {
-    
+    position: 'absolute',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    bottom: 0,
     width: 375,
     height: 549,
-    borderRadiusLeft: 8,
-    borderRadiusRight: 8,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
     backgroundColor: '#FFFFFF',
+  },
+  photo: {
+    borderColor: '#E8E8E8',
+    borderWidth: 1,
+    width: 120,
+    height: 120,
+    left: 128,
+    top: -60,
+    backgroundColor: `#F6F6F6`,
+    borderRadius: 16,
+    marginBottom: 32,
   }
 });
