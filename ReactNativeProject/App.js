2290ff60641d1ba933f674795d90a75fd9c87d2e
andrii-pozniak <andriipozniak@gmail.com>
2023-02-25 13:36:45 +0200

    1

---
+++ ReactNativeProject/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require("./assets/images/12.jpg")} style={styles.image}>
+     
+      </ImageBackground>
+      <StatusBar style="auto" /> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    resizeMode: "cover"
+
+
+  }
+});
