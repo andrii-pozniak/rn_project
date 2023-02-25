0959eedc9330983fa1da67995181c09fc0f62e88
andrii-pozniak <andriipozniak@gmail.com>
2023-02-25 22:05:03 +0200

    1

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require("./assets/images/12.jpg")} style={styles.image}>
-     
+      <ImageBackground source={require("./assets/images/photo_bg.jpg")} style={styles.image}>
+        <View style={styles.form}>
+        <Text style={styles.text}>Registration</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Login"
+        // onChangeText={onChangeText}
+        // value={text}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+        // onChangeText={onChangeText}
+        // value={text}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+        // onChangeText={onChangeText}
+        // value={text}
+        />
+        </View>
+        
       </ImageBackground>
-      <StatusBar style="auto" /> 
     </View>
   );
 }
@@ -16,14 +36,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 500,
+    marginBottom: 33,    
   },
   image: {
     flex: 1,
-    justifyContent: 'center',
-    resizeMode: "cover"
-
-
+    resizeMode: 'cover',
+  },
+  input: {
+    height: 50,
+    width: 343,
+    marginBottom: 16,
+    backgroundColor: '#F6F6F6',
+    borderColor: '#E8E8E8',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 16,
+    fontSize: 16,
+    fontWeight: 400,
+  },
+  form: {
+    
+    width: 375,
+    height: 549,
+    borderRadiusLeft: 8,
+    borderRadiusRight: 8,
+    backgroundColor: '#FFFFFF',
   }
 });
