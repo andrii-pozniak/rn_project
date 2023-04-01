270a4113845f72a607adb0c9fd66e1f32fec94bd
andrii-pozniak <andriipozniak@gmail.com>
2023-04-01 23:42:06 +0300

    add new pages

--- App.js
+++ App.js
@@ -4,23 +4,34 @@ import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 
 
-let customFonts = {
-  "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
-  "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
-  "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-};
+// let customFonts = {
+//   "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+//   "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+//   "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+//   "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+// };
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  const routing = useRoute(false)
+  const routing = useRoute(true)
 
   useEffect(() => {
-    (async () => await Font.loadAsync(customFonts))();
-    setFontsLoaded(true);
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+          "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+        });
+        setFontsLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadFonts();
   }, []);
-
   if (!fontsLoaded) {
     return null;
   }
